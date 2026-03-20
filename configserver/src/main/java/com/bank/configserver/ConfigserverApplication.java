9ac8fdf432c53535a267d84bb5eac7f6eeda78af
prabinswain <prabinswain027@gmail.com>
2026-03-21 01:21:12 +0530

    adding yml files of all 3 service inside config server and making changes as per requirement

--- configserver/src/main/java/com/bank/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/bank/configserver/ConfigserverApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
-//@EnableConfigServer
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigserverApplication {
 
