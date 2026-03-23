192ca73420c4e9e4126bb20cc294d18938e6d8d7
prabinswain <prabinswain027@gmail.com>
2026-03-23 19:42:19 +0530

    external config server change and automatic refresment in all the client services by webhook , it means any change to config server containing configuration files of  all services will be automatic refresh, we dont need to re-run again

--- Accounts/src/main/java/com/bank/Accounts/dto/AccountsContactInfoDto.java
+++ Accounts/src/main/java/com/bank/Accounts/dto/AccountsContactInfoDto.java
@@ -1,15 +1,18 @@
 package com.bank.Accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
-
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport ) {
+public class AccountsContactInfoDto{
+      private  String message;
+       private Map<String, String> contactDetails;
+       private  List<String> onCallSupport ;
 }
