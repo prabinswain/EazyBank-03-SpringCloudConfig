aa0a20ff877aa808d7f407aff8739d0c306ebfde
prabinswain <prabinswain027@gmail.com>
2026-03-23 19:44:42 +0530

    external config server change and automatic refresment in all the client services by webhook , it means any change to config server containing configuration files of  all services will be automatic refresh, we dont need to re-run again

--- Cards/src/main/java/com/bank/cards/dto/CardsContactInfoDto.java
+++ Cards/src/main/java/com/bank/cards/dto/CardsContactInfoDto.java
@@ -7,9 +7,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport ) {
+public class CardsContactInfoDto{
+        private  String message;
+        private Map<String, String> contactDetails;
+        private  List<String> onCallSupport ;
 }
--- Loans/src/main/java/com/loans/dto/LoansContactInfoDto.java
+++ Loans/src/main/java/com/loans/dto/LoansContactInfoDto.java
@@ -8,8 +8,10 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport ) {
+@Getter
+@Setter
+public class LoansContactInfoDto{
+        private  String message;
+        private Map<String, String> contactDetails;
+        private  List<String> onCallSupport ;
 }
